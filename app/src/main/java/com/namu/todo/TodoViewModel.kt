d3dde6df6dd963c59d7e2469a58df2a5afabdfb2
saeyeong <syeongppp@gmail.com>
2021-03-19 14:58:07 +0900

    뷰모델 추가

---
+++ app/src/main/java/com/namu/todo/TodoViewModel.kt
@@ -0,0 +1,23 @@
+package com.namu.todo
+
+import androidx.lifecycle.*
+import kotlinx.coroutines.launch
+
+class TodoViewModel(private val repository: TodoRepository): ViewModel() {
+
+    val allWords: LiveData<List<Todo>> = repository.allTodo.asLiveData()
+
+    fun insert(todo: Todo) = viewModelScope.launch {
+        repository.insert(todo)
+    }
+}
+
+class TodoViewModelFactory(private val repository: TodoRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(TodoViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return TodoViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
