3a0c3034cb1bb9ff729d8cc5f1495839a6067c34
saeyeong <syeongppp@gmail.com>
2021-03-19 12:19:56 +0900

    데이터베이스 싱글톤 추가

--- app/src/main/java/com/namu/todo/AppDatabase.kt
+++ app/src/main/java/com/namu/todo/AppDatabase.kt
@@ -1,9 +1,29 @@
 package com.namu.todo
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Todo::class], version = 1)
+@Database(entities = [Todo::class], version = 1, exportSchema = false)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun TodoDao(): TodoDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "todo_database"
+                ).build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
 }
