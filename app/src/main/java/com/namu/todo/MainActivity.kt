ccf7d49a5c070dbf26352703dab2d97743998c8b
saeyeong <syeongppp@gmail.com>
2021-03-22 15:05:49 +0900

    메인액티비티에 뷰모델 추가 어댑터 데이터 초기화

--- app/src/main/java/com/namu/todo/MainActivity.kt
+++ app/src/main/java/com/namu/todo/MainActivity.kt
@@ -3,11 +3,22 @@ package com.namu.todo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 
 class MainActivity : AppCompatActivity() {
+
+    private val todoViewModel: TodoViewModel by viewModels {
+        TodoViewModelFactory((application as TodoApplication).repository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        todoViewModel.allWords.observe(this, Observer { todo ->
+            todo?.let { todoViewModel.adapter?.addData(it) }
+        })
     }
 
     fun addTodo(view: View) {
